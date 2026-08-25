3591cdde2f8c3a6bed0a9c6e6238aecf1111de10
selva_vishva_M <selvavishva24@gmail.com>
2026-08-25 18:51:15 +0530

    Time: 0 ms (100%), Space: 42.6 MB (85.79%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDepth(String s) {
+          int count=0;
+          int max=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+            count++;
+            if(count>max)
+            max=count;
+            }
+            else if( ch==')'){
+                count--;
+            }
+        }
+        return max;
+    }
+}
