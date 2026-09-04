e9a95beed8dbfca1be4053a68cb53428d2f00aa9
selva_vishva_M <selvavishva24@gmail.com>
2026-09-04 18:21:48 +0530

    Time: 1 ms (99.79%), Space: 46.3 MB (49.36%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -17,3 +17,18 @@ class Solution {
         return -1;
     }
 }
+
+//My method only 70 test case pass 
+        // int sum=0;
+        // int n=nums[0];
+        // int min=0;
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]>k)
+        //         sum=n-nums[i];
+        //         if(min<=k)
+        //         min=sum;
+        //         sum=k;
+        //     }
+        // }
+        // return sum;
